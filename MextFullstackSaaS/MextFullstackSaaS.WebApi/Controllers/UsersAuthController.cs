6e76d4e4140c4c5faf02a2535eaa004506d1c8ad
Fatoş Özolcar <116491803+fatosozolcar@users.noreply.github.com>
2024-06-06 14:02:21 +0300

    dall-e-3 entegrated

--- MextFullstackSaaS/MextFullstackSaaS.WebApi/Controllers/UsersAuthController.cs
+++ MextFullstackSaaS/MextFullstackSaaS.WebApi/Controllers/UsersAuthController.cs
@@ -1,8 +1,6 @@
 using MediatR;
-using MextFullstackSaaS.Application.Features.UserAuth.Commands.ForgotPassword;
 using MextFullstackSaaS.Application.Features.UserAuth.Commands.Login;
 using MextFullstackSaaS.Application.Features.UserAuth.Commands.Register;
-using MextFullstackSaaS.Application.Features.UserAuth.Commands.ResetPassword;
 using MextFullstackSaaS.Application.Features.UserAuth.Commands.VerifyEmail;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,44 +8,23 @@ namespace MextFullstackSaaS.WebApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-
-public class UsersAuthController : ControllerBase
+public class UsersAuthController(ISender mediatr) : ControllerBase
 {
-    private readonly ISender _mediatr;
-
-    public UsersAuthController(ISender mediatr)
-    {
-        _mediatr = mediatr;
-    }
-
     [HttpPost("register")]
     public async Task<IActionResult> RegisterAsync(UserAuthRegisterCommand command, CancellationToken cancellationToken)
-    {
-        //throw new ArgumentNullException(command.FirstName, "First name is required");
-        return Ok(await _mediatr.Send(command, cancellationToken));
+    { 
+        return Ok(await mediatr.Send(command, cancellationToken));
     }
+    
     [HttpPost("login")]
-    public async Task<IActionResult> LoginAsync(UserAuthLoginCommand command, CancellationToken cancellationToken)
-    {
-        //throw new ArgumentNullException(command.FirstName, "First name is required");
-        return Ok(await _mediatr.Send(command, cancellationToken));
+    public async Task<IActionResult>LoginAsync(UserAuthLoginCommand command, CancellationToken cancellationToken)
+    { 
+        return Ok(await mediatr.Send(command, cancellationToken));
     }
+    
     [HttpGet("verify-email")]
-    public async Task<IActionResult> VerifyEmailAsync([FromQuery]UserAuthVerifyEmailCommand command, CancellationToken cancellationToken)
-    {
-        //throw new ArgumentNullException(command.FirstName, "First name is required");
-        return Ok(await _mediatr.Send(command, cancellationToken));
-    }
-
-    [HttpGet("forgot-password")]
-    public async Task<IActionResult> ForgotPasswordAsync([FromQuery] UserAuthForgotPasswordCommand command, CancellationToken cancellationToken)
-    {
-        return Ok(await _mediatr.Send(command, cancellationToken));
-    }
-
-    [HttpGet("reset-password")]
-    public async Task<IActionResult> ResetPasswordAsync([FromQuery] UserAuthResetPasswordCommand command, CancellationToken cancellationToken)
-    {
-        return Ok(await _mediatr.Send(command, cancellationToken));
+    public async Task<IActionResult>VerifyEmailAsync([FromQuery] UserAuthVerifyEmailCommand command, CancellationToken cancellationToken)
+    { 
+        return Ok(await mediatr.Send(command, cancellationToken));
     }
 }
